9b524ab059d3a61caae658d98fcd696ff40fb555
0xAgun <islamtahsan0@gmail.com>
2023-04-12 19:35:25 +0600

    initial commit

--- favihash/urls.py
+++ favihash/urls.py
@@ -19,6 +19,10 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -38,4 +42,6 @@ urlpatterns = [
     path('api/v1/', include('api.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
