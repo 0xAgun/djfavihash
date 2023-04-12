e8d0c488f2e2f7120fe94ca7cb501e015916ef49
0xAgun <islamtahsan0@gmail.com>
2023-04-12 23:06:32 +0600

    initial commit

--- favihash/settings.py
+++ favihash/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a1!$j=r-=#8mvq$&#c$dk7x7sbf(wxpw@m55=khws(t_(ulpqq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
